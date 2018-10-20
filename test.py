6831ef56723c23787da480e76b5dc6b5ebbf83d4
fangde <fangde>
2018-10-20 19:21:38 +0800

    add gcontext

---
+++ test.py
@@ -0,0 +1,46 @@
+graphContext={
+
+'vertex':{},
+
+
+'edges':{}
+    
+
+}
+
+
+class BasicLayer(object):
+
+    
+    def __init__(self,name):
+        self.name=name
+
+
+        graphContext[self.name]=self
+
+        print "basic"
+
+    def get_all_layers():
+
+        f
+
+
+
+class SimpleLayer(BasicLayer):
+
+    def __init__(self,name):
+        self.weight=100
+        super(SimpleLayer,self).__init__(name)
+
+
+
+if  __name__=='__main__':
+    
+
+    t=SimpleLayer('what')
+
+
+    print t
+
+
+    
